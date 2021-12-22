4c887b14665fdd708e6b4d87d0c4e87614a9f891
mikolaj.wittbrodt <m.witt@int.pl>
2021-12-22 09:38:26 +0100

    Basic migration and setup for active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
-class User < ActiveRecord::Base
-  
+class User < ActiveRecord::Base  
   extend Devise::Models #added this line to extend devise model
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
+
+  has_many_attached :pictures
 
   VALID_USERNAME_REGEX= /^(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/ix
   validates :username, presence: true, length: {minimum:3, maximum:26},
