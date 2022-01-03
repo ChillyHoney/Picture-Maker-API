39a8df9a1e9d3c792b29b316a7d41f62022b9df2
mikolaj.wittbrodt <m.witt@int.pl>
2022-01-03 10:14:02 +0100

    Added update account tests

--- spec/requests/session_spec.rb
+++ spec/requests/session_spec.rb
@@ -6,10 +6,15 @@ RSpec.describe "Session", :type => :request do
 		@user.confirm
 		@sign_in_url = '/api/v1/auth/sign_in' 
 		@sign_out_url = '/api/v1/auth/sign_out'
+		@update_url = '/api/v1/auth'
 		@login_params = {
-				email: @user.email,
-				password: @user.password
+      email: @user.email,
+      password: @user.password
 		}
+    @update_params = {
+      email: @user.email,
+      password: @user.password
+		}		   
 	end
 
 	describe 'POST /api/v1/auth/sign_in' do	
@@ -50,19 +55,44 @@ RSpec.describe "Session", :type => :request do
 	end
 
 	describe 'DELETE /api/v1/auth/sign_out' do		
-		before do
-			#login @user created in the before block in outer describe block
+		before do			
 			post @sign_in_url, params: @login_params, as: :json
-			@headers = {
-				'uid' => response.headers['uid'],
-				'client' => response.headers['client'],
-				'access-token' => response.headers['access-token']
-			}
+			 @headers = {
+      'access-token' => response.headers['access-token'],
+      'client' => response.headers['client'],
+      'uid' => response.headers['uid']
+    }
 		end
 
 		it 'returns status 200' do
 			delete @sign_out_url, headers: @headers
 			expect(response).to have_http_status(200)
-		end
-	end
+    end       		
+	end  
+
+  describe 'PATCH /api/v1/auth' do
+    before do
+      post @sign_in_url, params: @login_params, as: :json
+      @headers = {
+        'access-token' => response.headers['access-token'],
+        'client' => response.headers['client'],
+        'uid' => response.headers['uid']
+      }
+    end
+
+    it 'should update user email' do
+      patch "/api/v1/auth/", params: @update_params, headers: @headers
+      expect(response).to have_http_status(:success)
+    end
+
+	  it 'when invalid data to change' do
+	  	patch @update_url, headers: @headers
+	  	expect(response).to have_http_status(:unprocessable_entity)
+	  end
+
+    it "when can't find user" do
+	  	patch @update_url, params: @update_params	  
+	  	expect(response).to have_http_status(:not_found)
+	  end
+  end
 end
