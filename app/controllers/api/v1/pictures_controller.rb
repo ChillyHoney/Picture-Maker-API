9b5ef59d93ee8c9705cf053c9b857272bba9af23
mikolaj.wittbrodt <m.witt@int.pl>
2022-02-09 11:29:11 +0100

    Full funcionality of update and favourite photos

--- app/controllers/api/v1/pictures_controller.rb
+++ app/controllers/api/v1/pictures_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::PicturesController < ApplicationController
   include Rails.application.routes.url_helpers
   before_action :authenticate_api_user!
   before_action :id_require, only: [:update, :favourite, :destroy]
+  before_action :finding_picture_id, except: [:index, :create]
 
   def index
     pictures = current_api_user.pictures.with_attached_file.map do |p|
@@ -17,19 +18,18 @@ class Api::V1::PicturesController < ApplicationController
   end
 
   def show
-    picture_id = current_api_user.pictures.find(params[:id])
     render json: {
-      id: picture_id.id ,
-      filename: picture_id.file.filename.base,
-      description: picture_id.description,
-      is_favourite: picture_id.is_favourite,
-      created_at: picture_id.file.created_at.strftime("%Y-%m-%d %H:%M"),
-      url: rails_blob_url(picture_id.file)
+      id: @picture_id.id ,
+      filename: @picture_id.file.filename.base,
+      description: @picture_id.description,
+      is_favourite: @picture_id.is_favourite,
+      created_at: @picture_id.file.created_at.strftime("%Y-%m-%d %H:%M"),
+      url: rails_blob_url(@picture_id.file)
     }
   end
 
   def create
-    picture = Picture.new({:description => params[:description]})
+    picture = Picture.new({:description => params[:description], :is_favourite => params[:is_favourite]})
     picture.file.attach(io: File.open(params.require(:file)), filename: params.require(:filename))
     current_api_user.pictures << picture
 
@@ -43,21 +43,25 @@ class Api::V1::PicturesController < ApplicationController
   end
 
   def update
-    current_api_user.pictures.find(params[:id]).file.update!(filename: params.require(:filename))
-    current_api_user.pictures.find(params[:id]).update!(description: params[:description])
+    @picture_id.file.update!(filename: params.require(:filename))
+    @picture_id.update!(description: params[:description])
   end
 
   def favourite
-    current_api_user.pictures.find(params[:id]).update!(is_favourite: !current_api_user.pictures.find(params[:id]).is_favourite)
-    render json: { is_favourite: current_api_user.pictures.find(params[:id]).is_favourite }
+    @picture_id.update!(is_favourite: !@picture_id.is_favourite)
+    render json: { is_favourite: @picture_id.is_favourite }
   end
 
   def destroy
-    current_api_user.pictures.find(params[:id]).destroy
+    @picture_id.destroy
   end
 
   private
   def id_require
     params.require(:id)
+  end
+
+  def finding_picture_id
+    @picture_id = current_api_user.pictures.find(params[:id])
   end
 end
